ca071b19ac10cc1e880d2f8738acbd4e4b8c61c2
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-04-23 11:43:10 +0200

    Added first REST controller /JW

--- evider/src/main/java/com/example/BasicApplication.java
+++ evider/src/main/java/com/example/BasicApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BasicApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BasicApplication.class, args);
-	}
-        
-        public static void test() {
-            System.out.println("foo bar baz");
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(BasicApplication.class, args);
+    }
 }
---
+++ evider/src/main/java/com/example/TestController.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("/sayHello")
+    public String greeting() {
+        return "Hello from group 09!";
+    }
+}
