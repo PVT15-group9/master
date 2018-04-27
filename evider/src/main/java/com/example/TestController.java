26101d439cee79a8d24434501b28da129709d8a2
Joonteee <34379335+Joonteee@users.noreply.github.com>
2018-04-27 10:29:46 +0200

    Update TestController.java

--- evider/src/main/java/com/example/TestController.java
+++ evider/src/main/java/com/example/TestController.java
@@ -2,6 +2,7 @@ package com.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class TestController {
