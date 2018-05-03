64ed15d8dfef1a29a0a13760fda7d1f62cf93930
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-03 11:15:23 +0200

    Added JWT dependecies

--- evider/src/main/java/com/example/TestController.java
+++ evider/src/main/java/com/example/TestController.java
@@ -68,26 +68,9 @@ public class TestController {
         return json;
     }
 
-    @RequestMapping("/")
-    public String defaultRoute() {
-        return "This is the default route for group 09!";
-    }
-
-    @RequestMapping("/sayHello")
-    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        String hello = "Hello ";
-        String from = " from group 09!";
-        String retString = hello + name + from;
-        return retString;
-    }
-
-    @RequestMapping("/sayGoodBye")
-    public String goodBye() {
-        return "Good bye from group 09!";
-    }
-
-    @RequestMapping("/test")
-    public String test() {
-        return "A late night test!";
+    @RequestMapping("/jwt")
+    public String jwtTest() {
+        
+        return "";
     }
 }
