90200fd384f3841bcab8edd37db84a52082a623d
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-16 00:52:07 +0200

    Trying to fix configuration again

--- evider/src/main/java/com/example/BasicApplication1.java
+++ evider/src/main/java/com/example/BasicApplication1.java
@@ -1,28 +1,21 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-//@SpringBootApplication
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-public class BasicApplication extends SpringBootServletInitializer {
-
+@SpringBootApplication
+public class BasicApplication1 extends SpringBootServletInitializer {
+    
     public static void main(String[] args) {
-        SpringApplication.run(BasicApplication.class, args);
+        SpringApplication.run(applicationClass, args);
     }
-
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(applicationClass);
     }
 
-    private static Class<BasicApplication> applicationClass = BasicApplication.class;
+    private static Class<BasicApplication1> applicationClass = BasicApplication1.class;
 }
