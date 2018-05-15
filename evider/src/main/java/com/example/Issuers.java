f9282c2c883d83278f2d9b3f9801604ec1402540
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-16 01:09:46 +0200

    Trying to fix configuration again

--- evider/src/main/java/com/example/Issuers.java
+++ evider/src/main/java/com/example/Issuers.java
@@ -2,11 +2,14 @@ package com.example;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
 
 /**
  *
  * @author johe2765 Jonathan Heikel (Wening)
  */
+@Configuration
+@PropertySource(value = "classpath:application.properties")
 public class Issuers {
 
     @Value("#{PropertySplitter.map('${evide.issuers}')}")
