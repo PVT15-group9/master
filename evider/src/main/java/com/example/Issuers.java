8621dd130664fa3c9b787624e4bace1403df7d6f
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-16 01:50:36 +0200

    Trying to fix configuration again

--- evider/src/main/java/com/example/Issuers.java
+++ evider/src/main/java/com/example/Issuers.java
@@ -2,17 +2,24 @@ package com.example;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  *
  * @author johe2765 Jonathan Heikel (Wening)
  */
-@Component
+@Configuration
 public class Issuers {
 
     @Value("#{PropertySplitter.map('${evide.issuers}')}")
     private Map<String, String> issuers;
+    
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
     
     public Map<String, String> getIssuers() {
         return issuers;
