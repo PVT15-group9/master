08864e013a2cdcd004dec6c65dd033fcf88aad37
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-16 02:50:55 +0200

    Fixed property file for secret keys

--- evider/src/main/java/com/example/IssuersConfig.java
+++ evider/src/main/java/com/example/IssuersConfig.java
@@ -2,8 +2,6 @@ package com.example;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Service;
 
 /**
