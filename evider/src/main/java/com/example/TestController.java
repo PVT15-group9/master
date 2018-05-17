e8e1d044d78c6213fddcc82de8ed840b6bc39e30
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-17 14:32:37 +0200

    Fixed SQL query for /routes

--- evider/src/main/java/com/example/TestController.java
+++ evider/src/main/java/com/example/TestController.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.sql.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +23,29 @@ public class TestController {
 
     @Autowired
     private JWTDecoder jwtDecoder;
+
+    private String resultSetToJSON(ResultSet rs) {
+        SimpleModule module = new SimpleModule();
+        module.addSerializer(new ResultSetSerializer());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(module);
+
+        ObjectNode objectNode = objectMapper.createObjectNode();
+        // put the resultset in a containing structure
+        objectNode.putPOJO("results", rs);
+        // @TODO : How to get rid of the containing structure /JW
+
+        // generate all
+        StringWriter sw = new StringWriter();
+        try {
+            objectMapper.writeValue(sw, objectNode);
+        } catch (IOException e) {
+            return IOHelper.writeException(e);
+        }
+
+        return sw.toString();
+    }
 
     private String executeQueryAndPrintResult(String sql) {
         SimpleModule module = new SimpleModule();
@@ -63,23 +85,38 @@ public class TestController {
      */
     @CrossOrigin
     @RequestMapping(value = version + "routes", method = RequestMethod.GET, produces = "application/json")
-    public String getRoutesByVenue(@RequestHeader("Authorization") String authHeader, @RequestParam("user_value") String userValue) {
+    public String getRoutesByVenue(@RequestHeader("Authorization") String authHeader, @RequestParam("user_value") int userValue) {
         if (authHeader.length() < 7) {
             return "{\"error\" : \"Invalid Authorization header!\"}";
         }
         if (!authHeader.substring(0, 7).equals("Bearer ")) {
             return "{\"error\" : \"Malformed Authorization header!\"}";
         }
-        
+
         String token = authHeader.substring(7);
-        
+
         boolean decoded = jwtDecoder.decode(token);
-        if(!decoded) {
-            return "{\"error\" : \"jwt was not verified\"}";
+        if (!decoded) {
+            return "{\"error\" : \"jwt was not verified : " + token + "\"}";
+        }
+
+        cxn = db.connect();
+        String sql = "SELECT  r.id AS 'route_id', r.endpoint_id , e.transport_type,  e.name AS 'e_name', t.name AS 't_name', r.venue_id,  e.SL_SITE_ID, t.img_url AS 'icon', 'https://res.cloudinary.com/pvt-group09/image/upload/v1525786167/sensor-red.png' AS 'crowd_indicator', r.distance_in_meters, r.color, r.color_hex, CONCAT(ROUND(((r.distance_in_meters / 1000) / 5) * 60), \" min\") AS 'time'  FROM routes r  JOIN venues v ON r.venue_id = v.id JOIN endpoints e ON r.endpoint_id = e.id JOIN transport_types t ON e.transport_type = t.id WHERE r.venue_id = ?  ORDER BY r.distance_in_meters ASC";
+
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = cxn.prepareStatement(sql);
+            stmt.setInt(1, userValue);
+            rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            return "{\"error\" : \"error in sql\"}";
         }
         
-        return "{\"user value from url\" : \"" + userValue + "\"}";
-        //return jwtDecoder.getPayload(token);
+        String output = this.resultSetToJSON(rs);
+        db.disconnect();
+        return output;
     }
 
     @RequestMapping(value = version + "venues", method = RequestMethod.GET, produces = "application/json")
