7cc4111dfdc88793ba1828fecb433f8aa50f4699
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-17 17:16:51 +0200

    Added tweet route

--- evider/src/main/java/com/example/TestController.java
+++ evider/src/main/java/com/example/TestController.java
@@ -113,7 +113,7 @@ public class TestController {
         } catch (SQLException e) {
             return "{\"error\" : \"error in sql\"}";
         }
-        
+
         String output = this.resultSetToJSON(rs);
         db.disconnect();
         return output;
@@ -170,6 +170,44 @@ public class TestController {
     @RequestMapping("/")
     public String troll() {
         return "All your scrum are belong to us";
+    }
+
+    @RequestMapping("/tweet")
+    public String tweet() {
+        cxn = db.connect();
+        /*
+            SELECT r.color, r.distance_in_meters, v.name AS 'v_name', e.name AS 'e_name', t.name AS 't_name'
+            FROM routes r
+            JOIN venues v ON r.venue_id = v.id
+            JOIN endpoints e ON r.endpoint_id = e.id
+            JOIN transport_types t ON e.transport_type = t.id
+            ORDER BY RAND()
+            LIMIT 1
+        */
+        String sql = "SELECT r.color, r.distance_in_meters, v.name AS 'v_name', e.name AS 'e_name', t.name AS 't_name' FROM routes r JOIN venues v ON r.venue_id = v.id JOIN endpoints e ON r.endpoint_id = e.id JOIN transport_types t ON e.transport_type = t.id ORDER BY RAND() LIMIT 1";
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        String output = "";
+
+        try {
+            stmt = cxn.prepareStatement(sql);
+            rs = stmt.executeQuery();
+            while (rs.next()) {
+                String color = rs.getString("color");
+                int distance = rs.getInt("distance_in_meters");
+                String venue = rs.getString("v_name");
+                String endpoint = rs.getString("e_name");
+                String transportType = rs.getString("t_name");
+                
+                output += "Here's a tip! Go from " + venue + " to " + endpoint + " (" + transportType + ") by following the " + color + " lights! It's only " + distance + " meters";
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            return "{\"error\" : \"error in sql\"}";
+        }
+        db.disconnect();
+
+        return output;
     }
 
     @RequestMapping("/venues")
