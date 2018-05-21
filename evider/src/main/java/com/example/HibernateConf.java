dbaeaa33b8981e6f8720344cc05834bbe36032ca
d11d <vip_stockholm2014@hotmail.se>
2018-05-22 00:38:24 +0200

    Ny version med sensor

---
+++ evider/src/main/java/com/example/HibernateConf.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+/**
+ * Håller upp kopplingen mellan en databas och ett API.
+ * @author Dmitri
+ *
+ */
+@Configuration
+@EnableTransactionManagement
+public class HibernateConf {
+ 
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() {
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan(new String[] { "com.sensor.model" });
+        sessionFactory.setHibernateProperties(hibernateProperties());
+ 
+        return sessionFactory;
+    }
+ 
+    @Bean
+    public DataSource dataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://mysql.dsv.su.se:3306/bekn5739");
+        dataSource.setUsername("bekn5739");
+        dataSource.setPassword("iw8seeCh8ag9");
+        return dataSource;
+    }
+ 
+    @Bean
+    public PlatformTransactionManager hibernateTransactionManager() {
+        HibernateTransactionManager transactionManager
+          = new HibernateTransactionManager();
+        transactionManager.setSessionFactory(sessionFactory().getObject());
+        return transactionManager;
+    }
+ 
+    private final Properties hibernateProperties() {
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty(
+                "hibernate.hbm2ddl.auto", "create-drop");
+        hibernateProperties.setProperty(
+          "hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+ 
+        return hibernateProperties;
+    }
+}
