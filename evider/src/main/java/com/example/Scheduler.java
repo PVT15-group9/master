48135c59d779747d2d5688ca1f26a8e0c38ab484
Jonathan <Jonathan@LAPTOP-661VDUAR>
2018-05-23 11:41:07 +0200

    Removed duplicate code from the scheduled tweet

--- evider/src/main/java/com/example/Scheduler.java
+++ evider/src/main/java/com/example/Scheduler.java
@@ -49,7 +49,6 @@ public class Scheduler {
 
     public String tweetEvent() {
         TwitterHelper th = new TwitterHelper();
-        Twitter twitter = new TwitterTemplate(twitterConfig.getConsumerKey(), twitterConfig.getConsumerSecret(), twitterConfig.getAccessToken(), twitterConfig.getAccessTokenSecret());
 
         cxn = db.connect();
         String sql = "SELECT v.name AS 'venue_name', e.name AS 'event_name', e.doors_time, e.start_time, e.end_time, e.event_url FROM events e JOIN venues v ON e.venue_id = v.id WHERE DATE(start_time) = CURRENT_DATE() OR DATE(doors_time) = CURRENT_DATE()";
@@ -73,13 +72,6 @@ public class Scheduler {
                 String output = "At " + venueName + " today: " + eventName + ". Doors at " + doorsTime + ", events starts at: " + startTime;
                 if (!twitterHelper.makeTweet(output)) {
                     return "Error when making tweet!";
-                }
-
-                try {
-                    twitter.timelineOperations().updateStatus(output);
-                } catch (RuntimeException ex) {
-                    // log the error
-                    return "Could not tweet : " + ex.getMessage();
                 }
             }
             stmt.close();
